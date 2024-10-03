56458c5210d73e74a2beb9270220a82da85a8475
Ayoon Butt <111302380+AyoonButt@users.noreply.github.com>
2024-10-02 19:08:57 -0500

    Create SpringBoot application

---
+++ postgres/src/main/kotlin/com/example/api/postgres/PostgresApplication.kt
@@ -0,0 +1,42 @@
+package com.example.api.postgres
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PostgresApplication
+
+fun main(args: Array<String>) {
+	runApplication<PostgresApplication>(*args)
+}
+
+fun connectToDatabase() {
+    try {
+        Database.connect(
+            url = "jdbc:postgresql://recommendation-app.c76yuoa4ivv0.us-east-2.rds.amazonaws.com:5432/postgres",
+            driver = "org.postgresql.Driver",
+            user = "postgres",  // e.g., "postgres"
+            password = "Watermelon2131"  // your local database password
+        )
+    } catch (e: Exception) {
+        Log.e("DatabaseConnection", "Connection to local DB failed: ${e.message}", e)
+    }
+}
+
+
+fun createTables() {
+    transaction {
+        addLogger(StdOutSqlLogger)
+        SchemaUtils.create(
+            Users,
+            Posts,
+            UserPostInteractions,
+            UserTrailerInteractions,
+            Comments,
+            SubscriptionProviders,
+            Genres,
+            UserSubscriptions,
+            PostGenres
+        )
+    }
+}
